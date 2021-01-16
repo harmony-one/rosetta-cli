4df567524402b01ed47ceaf56dc774553cbba654
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2021-01-15 16:02:59 -0800

    [parse] DEBUG

--- pkg/parse/supply.go
+++ pkg/parse/supply.go
@@ -87,6 +87,7 @@ func (t *SupplyParser) StartPruning(
 }
 
 func (t *SupplyParser) PruneableIndex(ctx context.Context, headIndex int64) (int64, error) {
+	fmt.Printf("attempting to prune...\n")
 	safestBlockToRemove := headIndex - t.config.MaxSyncConcurrency
 	if safestBlockToRemove < 0 {
 		safestBlockToRemove = 0
